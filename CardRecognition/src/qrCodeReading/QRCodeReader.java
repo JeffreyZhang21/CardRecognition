7dc54e3fb82273f6fd4262bfc8f00e314671394b
Jeffr <Jeffr@JefffreyLaptop>
2019-05-25 12:02:52 -0400

    Stuff

---
+++ CardRecognition/src/qrCodeReading/QRCodeReader.java
@@ -0,0 +1,55 @@
+package qrCodeReading;
+
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.HybridBinarizer;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class QRCodeReader {
+
+    private static String decodeQRCode(File qrCodeimage) throws IOException 
+    {
+        BufferedImage bufferedImage = ImageIO.read(qrCodeimage);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+//       bitmap = bitmap.crop(0, 0, bitmap.getWidth()/2, bitmap.getHeight());
+//        
+//    
+//        
+//        Path path = FileSystems.getDefault().getPath("test.png");
+//        try {
+//			MatrixToImageWriter.writeToPath( bitmap.getBlackMatrix(), "PNG", path);
+//		} catch (NotFoundException e1) {
+//			e1.printStackTrace();
+//		}
+        
+        try {
+            Result result = new MultiFormatReader().decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            System.out.println("There is no QR code in the image");
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            File file = new File("QrCodes//1 of clovers.png");
+            String decodedText = decodeQRCode(file);
+            if(decodedText == null) {
+                System.out.println("No QR Code found in the image");
+            } else {
+                System.out.println("Decoded text = " + decodedText);
+            }
+        } catch (IOException e) {
+            System.out.println("Could not decode QR Code, IOException :: " + e.getMessage());
+        }
+    }
+}
